b2728c608c53636132d06f924cbf2e7868ff5029
frankielin2007 <frankielin2007@gmail.com>
2025-11-10 23:17:14 -0500

    Partner 2 test cases

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,20 @@
+# https://github.com/PokeKoa/lab10-MQ-FL
+# Partner 1: Mike Quick
+# Partner 2: Frankie Lin
 import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self):
+        self.assertEqual(add(3,4),7)
+        self.assertEqual(add(-1,-1),-2)
+        self.assertEqual(add(0,0),0)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self):
+        self.assertEqual(sub(11,5),6)
+        self.assertEqual(sub(-1,-1),0)
+        self.assertEqual(sub(0,5),-5)
+
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -19,19 +25,20 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            divide(5,0)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self):
+        self.assertEqual(log(27,3),3)
+        self.assertEqual(log(1, 10),0)
+        self.assertAlmostEqual(log(100,10),2)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+
+
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            lo(8, -1)
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
